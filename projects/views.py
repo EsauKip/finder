00e50d02a6a2bfa4a105b546840d717fadd91b3d
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-15 23:01:43 +0300

    create sign up function

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,9 @@
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+def signup(request):
+    return render(request, 'authentication/signup.html')
+
