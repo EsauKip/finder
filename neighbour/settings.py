8b46c4c0ed141e5b980c14531c36c1a76bd53007
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-15 21:28:14 +0300

    configure settings

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neighbour.apps.NeighbourConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -96,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
