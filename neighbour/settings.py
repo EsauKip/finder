8752bbfed6c89e2894424c769c7d27f51b06d452
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-16 20:53:47 +0300

    ceate dashboard to show neighborhood

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -69,8 +69,10 @@ WSGI_APPLICATION = 'neighbour.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+          'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'post',
+        'USER': 'moringa',
+        'PASSWORD':'kiprono12',
     }
 }
 
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from .models import Neighborhood, Post, Profile, HoodMember,Business,Service
 def index(request):
     return render(request, 'index.html')
+
+#show neighborhood
+def dashboard(request):
+    hoods=Neighborhood.objects.all()
+    context={'hoods':hoods}
+    return render(request,'dashboard.html',context)
