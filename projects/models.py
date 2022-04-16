78c325cfcb3aa2d21d95d93d8613e290b3241fbe
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-16 20:20:15 +0300

    create profile class,save profile method

--- projects/models.py
+++ projects/models.py
@@ -101,5 +101,21 @@ class Service(models.Model):
     
     def __str__(self):
         return self.name
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField(max_length=500,blank=True)
+    profile_pic = CloudinaryField('image')
+    
+    
+    def __str__(self):  
+        return str(self.user)
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
