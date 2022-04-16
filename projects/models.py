9d7fb1f1c3315a1e657e815bbe8a4496c1fefe38
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-16 20:18:56 +0300

    create service class

--- projects/models.py
+++ projects/models.py
@@ -91,3 +91,15 @@ class Business(models.Model):
     def search_business(self,key_word):
         businesses = Business.objects.filter(name__icontains=key_word)
         return businesses
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=500,default=None)
+    Neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.name
+
+
